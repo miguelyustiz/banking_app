ae387a4a04587a001f03df907860001aeaa0e4a2
miguelyustiz <154564029+miguelyustiz@users.noreply.github.com>
2024-08-03 21:05:40 -0500

    HeaderBox and TotalBalanceBox created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/ui/HeaderBox";
+import { log } from "console";
 import React from "react";
 
 const Home = () => {
-  return <div>page</div>;
+  const logIn = {
+    firstName: "Miguel",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={logIn.firstName || "Guest"}
+            subtext="Acces and mange your account and transactions succesfully"
+          />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.82}/>
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
